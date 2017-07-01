8ad6a92dbd2beda3c4bac67fa616808d3029ee2e
oHSUANo <chhu0830@gmail.com>
2017-07-01 13:34:04 +0800

    Add boundary of boards

--- board.js
+++ board.js
@@ -1,22 +1,25 @@
-var board = {
-    position: null,
+var board = { position: null,
     length: 100,
     up: false,
     down: false,
     speed: 0,
+    min_pos: 0,
+    max_pos: 0,
 
-    create: function(x, y, length, speed) {
+    create: function(x, y, length, speed, min_pos, max_pos) {
         var obj = Object.create(this);
         obj.position = vector.create(x, y);
-        obj.length = length;
+        obj.length = length / 2;
         obj.speed = speed;
+        obj.min_pos = min_pos + obj.length;
+        obj.max_pos = max_pos - obj.length;
         return obj
     },
 
     draw: function(context) {
         context.beginPath();
-        context.moveTo(this.position.getX(), this.position.getY() - this.length / 2);
-        context.lineTo(this.position.getX(), this.position.getY() + this.length / 2);
+        context.moveTo(this.position.getX(), this.position.getY() - this.length);
+        context.lineTo(this.position.getX(), this.position.getY() + this.length);
         context.stroke();
     },
 
@@ -37,10 +40,10 @@ var board = {
     },
 
     update: function() {
-        if (this.up) {
+        if (this.up && this.position.getY() > this.min_pos) {
             this.moveUp();
         }
-        if (this.down) {
+        if (this.down && this.position.getY() < this.max_pos) {
             this.moveDown();
         }
     }
--- game.js
+++ game.js
@@ -9,11 +9,13 @@ window.onload = function() {
     context.lineWidth = 10;
 
     var ball_speed = 5;
-    var board_speed = 5;
+    var board_speed = 10;
+    var board_length = height * (2 / 10);
+    var min_pos = 0, max_pos = height;
 
     var ball0  = ball.create(width / 2, height / 2, 20, ball_speed, Math.random() * 180);
-    var board1 = board.create(10, height / 2, height * (2 / 10), board_speed);
-    var board2 = board.create(width - 10, height / 2, height * (2 / 10), board_speed);
+    var board1 = board.create(10, height / 2, board_length, board_speed, min_pos, max_pos);
+    var board2 = board.create(width - 10, height / 2, board_length, board_speed, min_pos, max_pos);
 
 
     document.body.addEventListener("keydown", function(event) {
