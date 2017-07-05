6592c186ce2bb93ef09e22d44b2b4c9c48c3e0bb
oHSUANo <chhu0830@gmail.com>
2017-07-05 09:02:02 +0800

    Merge check function and bounce function for ball

--- ball.js
+++ ball.js
@@ -2,7 +2,7 @@ var ball = {
     position: null,
     radius: 0,
     velocity: null,
-    route: null,
+    direction: null,
     func: null,
     visible: true,
     t: 0,
@@ -15,9 +15,8 @@ var ball = {
         obj.velocity = vector.create(x, y);
         obj.velocity.setLength(speed);
         obj.velocity.setAngle(direction);
-        obj.route = vector.create(x, y);
-        obj.route.setLength(speed);
-        obj.route.setAngle(direction);
+        obj.direction = vector.create(x, y);
+        obj.direction.setAngle(direction);
         obj.func = func;
         return obj
     },
@@ -31,51 +30,56 @@ var ball = {
     },
 
     update: function() {
-        this.position.setX(this.position.getX() + this.route.getX());
-        this.position.setY(this.position.getY() + this.route.getY());
+        this.position.setX(this.position.getX() + this.velocity.getX());
+        this.position.setY(this.position.getY() + this.velocity.getY());
 
         angle = Math.atan2(this.func(this.t + this.dx) - this.func(this.t), this.dx);
-        this.route.setAngle(this.velocity.getAngle() + angle);
+        this.velocity.setAngle(this.direction.getAngle() + angle);
         this.t++;
     },
 
-    check: function(context) {
-        width = context.canvas.width;
-        height = context.canvas.height;
-
-        if (this.position.getY() + this.route.getY() - this.radius <= 0 || this.position.getY() + this.route.getY() + this.radius >= height)
-            this.reverse("Y")
-
-        if (this.position.getX() + this.route.getX() - this.radius <= 0) {
-            this.reverse("X")
-            return -1;
+    check: function() {
+        if (this.position.getY() + this.velocity.getY() - this.radius <= 0) {
+            this.position.setY(0 + this.velocity.getY() + this.radius);
+            this.reverse("Y");
         }
-        else if (this.position.getX() + this.route.getX() + this.radius >= width) {
-            this.reverse("X")
-            return 1;
+        else if (this.position.getY() + this.velocity.getY() + this.radius >= height) {
+            this.position.setY(height + this.velocity.getY() - this.radius);
+            this.reverse("Y");
         }
-        else {
-            return 0;
-        }
-    },
 
-    bounce: function(board) {
-        if (this.position.getY() >= board.min &&
-            this.position.getY() <= board.max &&
-            Math.abs(this.position.getX() + this.route.getX() - board.position.getX()) < this.radius) {
+        if (this.position.getY() >= board1.min && this.position.getY() <= board1.max && this.position.getX() + this.velocity.getX() - board1.position.getX() < this.radius) {
+            this.position.setX(board1.position.getX() + this.velocity.getX() + this.radius);
             this.reverse("X");
         }
+        else if (this.position.getY() >= board2.min && this.position.getY() <= board2.max && board2.position.getX() - (this.position.getX() + this.velocity.getX()) < this.radius) {
+            this.position.setX(board2.position.getX() + this.velocity.getX() - this.radius);
+            this.reverse("X");
+        }
+        else {
+            if (this.position.getX() + this.velocity.getX() - this.radius <= 0) {
+                this.position.setX(0 + this.velocity.getX() + this.radius);
+                this.reverse("X")
+                return -1;
+            }
+            else if (this.position.getX() + this.velocity.getX() + this.radius >= width) {
+                this.position.setX(width + this.velocity.getX() - this.radius);
+                this.reverse("X")
+                return 1;
+            }
+        }
+        return 0;
     },
 
     reverse: function(axis) {
         switch (axis) {
             case "X":
+                this.direction.setX(-this.direction.getX());
                 this.velocity.setX(-this.velocity.getX());
-                this.route.setX(-this.route.getX());
                 break;
             case "Y":
+                this.direction.setY(-this.direction.getY());
                 this.velocity.setY(-this.velocity.getY());
-                this.route.setY(-this.route.getY());
                 break;
             default:
                 break;
