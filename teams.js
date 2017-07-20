770b669953508e759e2f2b8d47c0f06629ae2a92
oHSUANo <chhu0830@gmail.com>
2017-07-20 08:08:40 +0800

    Revise teams.js

--- teams.js
+++ teams.js
@@ -3,14 +3,16 @@ var mySkill = {
     icon: "./icon.jpg",
     time: 5,
     func: function(player) {
-        case 0:
-            // 如果我是左邊的玩家，我想要...
-            break;
-        case 1:
-            // 如果我是右邊的玩家，我想要...
-            break;
-        default:
-            // 技能結束後，我希望...
-            break;
+        switch(player) {
+            case 0:
+                // 如果我是左邊的玩家，我想要...
+                break;
+            case 1:
+                // 如果我是右邊的玩家，我想要...
+                break;
+            default:
+                // 技能結束後，我希望...
+                break;
+        }
     }
 }
